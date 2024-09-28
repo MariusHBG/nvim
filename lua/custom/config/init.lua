8fa4b0f98f7469a080c3eae11fe00849906c0a6b
Marius <mgruber@web.de>
2024-09-28 23:28:07 +0200

    Moved remaps to own remap.lua

--- lua/custom/config/init.lua
+++ lua/custom/config/init.lua
@@ -1,9 +1,1 @@
--- Put debug output here
-vim.keymap.set('n', '<leader>md', function()
-  --  print(string.format('%s/Tools/zls', vim.env.HOME))
-end)
-
--- Disabled in favor of harpoon keymap
--- vim.keymap.set('n', '<C-s>', vim.cmd.write)
-
-vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
+require 'custom/config/remap'
---
+++ lua/custom/config/remap.lua
@@ -0,0 +1,11 @@
+-- Put debug output here
+vim.keymap.set('n', '<leader>md', function()
+  --  print(string.format('%s/Tools/zls', vim.env.HOME))
+end)
+
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
+
+-- Disabled in favor of harpoon keymap
+-- vim.keymap.set('n', '<C-s>', vim.cmd.write)
+
+-- TODO: jump between current and previous file
