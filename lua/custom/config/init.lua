e856a3b88ed9e3fe193194c46d0bfc8ad258a97f
Marius <mgruber@web.de>
2024-09-28 18:15:57 +0200

    Added harpoon

--- lua/custom/config/init.lua
+++ lua/custom/config/init.lua
@@ -3,6 +3,7 @@ vim.keymap.set('n', '<leader>md', function()
   --  print(string.format('%s/Tools/zls', vim.env.HOME))
 end)
 
-vim.keymap.set('n', '<C-s>', vim.cmd.write)
+-- Disabled in favor of harpoon keymap
+-- vim.keymap.set('n', '<C-s>', vim.cmd.write)
 
 vim.keymap.set('n', '<leader>w', vim.cmd.Ex)
