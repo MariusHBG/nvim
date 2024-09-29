c82271bdecece62868670edbdf6e4a94d58f2a72
Marius <mgruber@web.de>
2024-09-29 15:00:24 +0200

    Added [P]roject search group to which key root menu

--- lua/plugins/which_key.lua
+++ lua/plugins/which_key.lua
@@ -63,6 +63,7 @@ return {
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
         { '<leader>w', group = '[W]orkspace' },
+        { '<leader>p', group = '[P]roject search' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       },
