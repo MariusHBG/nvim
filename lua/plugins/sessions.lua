f1f67db8c5e433ebd5c9d839d5a200dc689e019b
Marius <mgruber@web.de>
2024-10-12 15:29:39 +0200

    Configured autosession to open nvim-tree after restoring session

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -1,7 +1,17 @@
 return {
   'rmagatti/auto-session',
   config = function()
-    require('auto-session').setup()
+    require('auto-session').setup {
+      post_restore_cmds = {
+        function()
+          -- restore nvim-tree after a session is restored
+          local nvim_tree_api = require 'nvim-tree.api'
+          nvim_tree_api.tree.open()
+          nvim_tree_api.tree.change_root(vim.fn.getcwd())
+          nvim_tree_api.tree.reload()
+        end,
+      },
+    }
     vim.keymap.set('n', '<Leader>ls', require('auto-session.session-lens').search_session, { noremap = true, desc = '[L]ist current [S]essions' })
   end,
   opts = {
