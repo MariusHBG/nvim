6dcfdf87239e8347e67457541b3a0632371b0208
Marius <mgruber@web.de>
2024-10-26 01:37:39 +0200

    Added rainbox delimiters

---
+++ lua/plugins/rainbox-delimiters.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    'HiPhish/rainbow-delimiters.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    init = function()
+      local rainbow_delimiters = require 'rainbow-delimiters'
+
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [''] = rainbow_delimiters.strategy['global'],
+          vim = rainbow_delimiters.strategy['local'],
+        },
+        query = {
+          [''] = 'rainbow-delimiters',
+          lua = 'rainbow-blocks',
+          tsx = 'rainbow-parens',
+          javascript = 'rainbow-delimiters-react',
+        },
+        highlight = {
+          'RainbowDelimiterRed',
+          'RainbowDelimiterOrange',
+          'RainbowDelimiterYellow',
+          'RainbowDelimiterGreen',
+          'RainbowDelimiterBlue',
+          'RainbowDelimiterCyan',
+          'RainbowDelimiterViolet',
+        },
+      }
+    end,
+  },
+}
