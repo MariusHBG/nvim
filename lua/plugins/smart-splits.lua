f03222df1a9b728f88d5bc94d877f5d54ac1889c
Marius Gruber <marius_gr@web.de>
2024-12-14 12:58:39 +0100

    Add additional hjkl binds for smart splits

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -13,11 +13,22 @@ return {
       vim.keymap.set('n', '<A-Up>', require('smart-splits').resize_up)
       vim.keymap.set('n', '<A-Right>', require('smart-splits').resize_right)
 
+      vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
+      vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
+      vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
+      vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right)
+
       -- moving between splits
+      vim.keymap.set({ 'n', 'i', 'v' }, '<C-h>', require('smart-splits').move_cursor_left)
+      vim.keymap.set({ 'n', 'i', 'v' }, '<C-j>', require('smart-splits').move_cursor_down)
+      vim.keymap.set({ 'n', 'i', 'v' }, '<C-k>', require('smart-splits').move_cursor_up)
+      vim.keymap.set({ 'n', 'i', 'v' }, '<C-l>', require('smart-splits').move_cursor_right)
+
       vim.keymap.set({ 'n', 'i', 'v' }, '<C-Left>', require('smart-splits').move_cursor_left)
       vim.keymap.set({ 'n', 'i', 'v' }, '<C-Down>', require('smart-splits').move_cursor_down)
       vim.keymap.set({ 'n', 'i', 'v' }, '<C-Up>', require('smart-splits').move_cursor_up)
       vim.keymap.set({ 'n', 'i', 'v' }, '<C-Right>', require('smart-splits').move_cursor_right)
+
       vim.keymap.set({ 'n', 'i', 'v' }, '<C-\\>', require('smart-splits').move_cursor_previous)
 
       vim.keymap.set({ 'n', 'i', 'v' }, '<C-w>h', vim.cmd.split, { desc = 'Split window horizontally' })
@@ -27,6 +38,11 @@ return {
       vim.keymap.set({ 'n', 'i', 'v' }, '<C-w>s<Right>', smart_splits.swap_buf_right, { desc = 'Move window to the right' })
       vim.keymap.set({ 'n', 'i', 'v' }, '<C-w>s<Up>', smart_splits.swap_buf_up, { desc = 'Move window to the top' })
       vim.keymap.set({ 'n', 'i', 'v' }, '<C-w>s<Down>', smart_splits.swap_buf_down, { desc = 'Move window to the bottom' })
+
+      vim.keymap.set({ 'n', 'i', 'v' }, '<C-w>sh', smart_splits.swap_buf_left, { desc = 'Move window to the left' })
+      vim.keymap.set({ 'n', 'i', 'v' }, '<C-w>sl', smart_splits.swap_buf_right, { desc = 'Move window to the right' })
+      vim.keymap.set({ 'n', 'i', 'v' }, '<C-w>sk', smart_splits.swap_buf_up, { desc = 'Move window to the top' })
+      vim.keymap.set({ 'n', 'i', 'v' }, '<C-w>sj', smart_splits.swap_buf_down, { desc = 'Move window to the bottom' })
     end,
   },
 }
