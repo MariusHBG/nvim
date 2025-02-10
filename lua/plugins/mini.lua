9adccd6fd8652e3daeac07e80dad306713302933
Marius Gruber <marius_gr@web.de>
2025-02-10 22:43:37 +0100

    Explicitly enable mini.bufremove

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -33,6 +33,8 @@ return { -- Collection of various small independent plugins/modules
 
     require('mini.pairs').setup()
 
+    require('mini.bufremove').setup()
+
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
   end,
