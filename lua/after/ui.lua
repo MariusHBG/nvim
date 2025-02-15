100be2ad5fb2a196d535735027fa1a4f5f7da45c
Marius Gruber <marius_gr@web.de>
2025-02-15 21:24:05 +0100

    Fix diagnostics not appearing undercurled but underlined

--- lua/after/ui.lua
+++ lua/after/ui.lua
@@ -3,6 +3,10 @@ local symbols = { Error = '󰅙', Info = '󰋼', Hint = '󰌵', Warn = '' }
 for name, icon in pairs(symbols) do
   local hl = 'DiagnosticSign' .. name
   vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
-  local underline_hl = 'DiagnosticUnderline' .. name
-  vim.api.nvim_set_hl(0, underline_hl, { underline = true })
+
+  -- Forgot why I had this here in the first place, probably fixing
+  -- underline or undercurl in windows?
+  -- However, in linux it makes lsp diagnostics appear as underline and not undercurl
+  -- local underline_hl = 'DiagnosticUnderline' .. name
+  -- vim.api.nvim_set_hl(0, underline_hl, { underline = true })
 end
