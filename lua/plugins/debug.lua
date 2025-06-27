01cc05a0fbd6ef3f4edbfe45e188bf745ec6d729
Marius Gruber <mgruber@quantum-systems.com>
2025-06-27 20:47:37 +0200

    Fix lazy loading dap
    Remove comments

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -59,9 +59,7 @@ local function configure_dap_symbols()
 end
 
 return {
-  -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
@@ -77,8 +75,6 @@ return {
     'leoluz/nvim-dap-go',
   },
   keys = function(_, keys)
-    local dap = require 'dap'
-    local dapui = require 'dapui'
     return {
       {
         '<F5>',
@@ -92,26 +88,56 @@ return {
         desc = 'Start using launch.json',
       },
       -- { '<F5>', dap.continue, desc = 'Debug: Start/Continue' },
-      { '<F11>', dap.step_into, desc = 'Debug: Step Into' },
-      { '<F10>', dap.step_over, desc = 'Debug: Step Over' },
-      { '<S-F11>', dap.step_out, desc = 'Debug: Step Out' },
-      { '<leader>db', dap.toggle_breakpoint, desc = 'Debug: Toggle Breakpoint' },
+      {
+        '<F11>',
+        function()
+          require('dap').step_into()
+        end,
+        desc = 'Debug: Step Into',
+      },
+      {
+        '<F10>',
+        function()
+          require('dap').step_over()
+        end,
+        desc = 'Debug: Step Over',
+      },
+      {
+        '<S-F11>',
+        function()
+          require('dap').step_out()
+        end,
+        desc = 'Debug: Step Out',
+      },
+      {
+        '<leader>db',
+        function()
+          require('dap').toggle_breakpoint()
+        end,
+        desc = 'Debug: Toggle Breakpoint',
+      },
       {
         '<leader>lp',
         function()
-          dap.set_breakpoint(nil, nil, vim.fn.input 'Log point message: ')
+          require('dap').set_breakpoint(nil, nil, vim.fn.input 'Log point message: ')
         end,
         desc = 'Debug: Insert log point',
       },
       {
         '<leader>dB',
         function()
-          dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+          require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
         end,
         desc = 'Debug: Set Breakpoint',
       },
       -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-      { '<F7>', dapui.toggle, desc = 'Debug: See last session result.' },
+      {
+        '<F7>',
+        function()
+          require('dapui').toggle()
+        end,
+        desc = 'Debug: See last session result.',
+      },
       -- { '<leader>gb', dap.run_to_cursor },
       unpack(keys),
     }
