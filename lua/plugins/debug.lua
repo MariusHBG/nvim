ba33d4634725ad181af4f3f2a518be315213e7e5
Marius Gruber <mgruber@quantum-systems.com>
2025-06-27 20:31:15 +0200

    Disable delve and dap-go setup

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -117,8 +117,6 @@ return {
     }
   end,
   config = function()
-    local dap = require 'dap'
-
     require('mason-nvim-dap').setup {
       -- Makes a best effort to setup the various debuggers with
       -- reasonable debug configurations
@@ -132,7 +130,7 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
+        -- 'delve', Disabled go as gopls failed to install and this makes startup slower
         'codelldb',
       },
     }
@@ -141,13 +139,13 @@ return {
     configure_dap_symbols()
 
     -- Install golang specific config
-    require('dap-go').setup {
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
-      },
-    }
+    -- require('dap-go').setup {
+    --   delve = {
+    --     -- On Windows delve must be run attached or it crashes.
+    --     -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
+    --     detached = vim.fn.has 'win32' == 0,
+    --   },
+    -- }
     -- dap.configurations.cpp = {
     --   {
     --     name = 'Launch file',
