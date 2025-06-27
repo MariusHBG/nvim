0606a8cd9ba89e2079ce31000b9cdec2da601ce0
Marius Gruber <mgruber@quantum-systems.com>
2025-06-28 00:02:54 +0200

    Fix nvim tree not loading keybindings

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,6 +2,14 @@ return {
   'nvim-tree/nvim-tree.lua',
   lazy = true,
   dependencies = 'nvim-tree/nvim-web-devicons',
+  keys = {
+    { '<leader>ee', '<cmd>NvimTreeToggle<CR>', desc = 'Toggle file explorer' },
+    { '<leader>ee', '<cmd>NvimTreeToggle<CR>', desc = 'Toggle file explorer' },
+    { '<leader>ef', '<cmd>NvimTreeFindFile<CR>', desc = 'Toggle file explorer on current file' },
+    { '<leader>ec', '<cmd>NvimTreeCollapse<CR>', desc = 'Collapse file explorer' },
+    { '<leader>er', '<cmd>NvimTreeRefresh<CR>', desc = 'Refresh file explorer' },
+  },
+
   config = function()
     local nvimtree = require 'nvim-tree'
     vim.g.loaded_netrw = 1
@@ -39,12 +47,5 @@ return {
         ignore = false,
       },
     }
-
-    -- set keymaps
-    local keymap = vim.keymap
-    keymap.set('n', '<leader>ee', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle file explorer' })
-    keymap.set('n', '<leader>ef', '<cmd>NvimTreeFindFile<CR>', { desc = 'Toggle file explorer on current file' })
-    keymap.set('n', '<leader>ec', '<cmd>NvimTreeCollapse<CR>', { desc = 'Collapse file explorer' })
-    keymap.set('n', '<leader>er', '<cmd>NvimTreeRefresh<CR>', { desc = 'Refresh file explorer' })
   end,
 }
