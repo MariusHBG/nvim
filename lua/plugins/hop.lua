b5f576aca20385598052787135d6c60b508438a1
Marius Gruber <marius_gr@web.de>
2025-06-29 15:45:07 +0200

    Replace flash with hop

---
+++ lua/plugins/hop.lua
@@ -0,0 +1,14 @@
+return {
+  'smoka7/hop.nvim',
+  version = '*',
+  opts = {
+    keys = 'etovxqpdygfblzhckisuran',
+  },
+  keys = {
+    {
+      'gw',
+      '<cmd>HopWord<CR>',
+      desc = 'Go to word in buffer',
+    },
+  },
+}
