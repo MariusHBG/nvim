97f9175e7cc49a931729688effabdfd795789c63
Marius Gruber <mgruber@quantum-systems.com>
2025-08-20 22:53:27 +0200

    Migrate diagnostic sign defines to new api

--- lua/after/ui.lua
+++ lua/after/ui.lua
@@ -1,12 +1,10 @@
-local symbols = { Error = '󰅙', Info = '󰋼', Hint = '󰌵', Warn = '' }
-
-for name, icon in pairs(symbols) do
-  local hl = 'DiagnosticSign' .. name
-  vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
-
-  -- Forgot why I had this here in the first place, probably fixing
-  -- underline or undercurl in windows?
-  -- However, in linux it makes lsp diagnostics appear as underline and not undercurl
-  -- local underline_hl = 'DiagnosticUnderline' .. name
-  -- vim.api.nvim_set_hl(0, underline_hl, { underline = true })
-end
+vim.diagnostic.config {
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '󰅙',
+      [vim.diagnostic.severity.WARN] = '',
+      [vim.diagnostic.severity.INFO] = '󰋼',
+      [vim.diagnostic.severity.HINT] = '󰌵',
+    },
+  },
+}
