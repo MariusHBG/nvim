53774423f64375d2a5e86ef49fdbe07dd62771b7
Marius Gruber <marius_gr@web.de>
2025-09-07 12:00:17 +0200

    Enable lazy loading neotest with keybinds

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -7,61 +7,101 @@ return {
     'antoinemadec/FixCursorHold.nvim',
     'nvim-treesitter/nvim-treesitter',
   },
+  keys = {
+    {
+      '<leader>tn',
+      function()
+        require('neotest').run.run()
+      end,
+      desc = '[T]est [n]earest',
+    },
+    {
+      '<leader>tk',
+      function()
+        require('neotest').run.stop()
+      end,
+      desc = '[T]est [k]ill run',
+    },
+    {
+      '<leader>tf',
+      function()
+        require('neotest').run.run(vim.fn.expand '%')
+      end,
+      desc = '[T]est [f]ile',
+    },
+    {
+      '<leader>tl',
+      function()
+        require('neotest').run.run_last()
+      end,
+      desc = '[T]est [l]ast',
+    },
+    {
+      '<leader>td',
+      function()
+        ---@diagnostic disable-next-line: missing-fields
+        require('neotest').run.run { strategy = 'dap' }
+      end,
+      desc = '[T]est [d]ebug nearest',
+    },
+
+    {
+      '<leader>to',
+      function()
+        require('neotest').output.open { enter = true, auto_close = true }
+      end,
+      desc = '[T]est open [o]utput',
+    },
+
+    {
+      '<leader>tso',
+      function()
+        require('neotest').summary.open()
+      end,
+      desc = '[T]est [s]ummary [o]pen',
+    },
+
+    {
+      '<leader>tsc',
+      function()
+        require('neotest').summary.close()
+      end,
+      desc = '[T]est [s]ummary [c]lose',
+    },
+
+    {
+      '<leader>tst',
+      function()
+        require('neotest').summary.close()
+      end,
+      desc = 'Toggle summary',
+    },
+
+    {
+      '<leader>tww',
+      function()
+        require('neotest').watch.watch(vim.fn.expand '%')
+      end,
+      desc = 'Watch file',
+    },
+
+    {
+      '<leader>tws',
+      function()
+        ---@diagnostic disable-next-line: missing-parameter
+        require('neotest').watch.stop()
+      end,
+      desc = 'Watch file',
+    },
+  },
   config = function()
-    local neotest = require 'neotest'
     ---@diagnostic disable-next-line: missing-fields
-    neotest.setup {
+    require('neotest').setup {
       -- ...,
       adapters = {
         -- ...,
         require 'rustaceanvim.neotest',
       },
     }
-
-    vim.keymap.set('n', '<leader>tn', function()
-      neotest.run.run()
-    end, { desc = 'Test nearest' })
-
-    vim.keymap.set('n', '<leader>tk', function()
-      neotest.run.stop()
-    end, { desc = 'Kill test run' })
-
-    vim.keymap.set('n', '<leader>tf', function()
-      neotest.run.run(vim.fn.expand '%')
-    end, { desc = 'Test file' })
-
-    vim.keymap.set('n', '<leader>tl', function()
-      neotest.run.run_last()
-    end, { desc = 'Test last' })
-
-    vim.keymap.set('n', '<leader>td', function()
-      ---@diagnostic disable-next-line: missing-fields
-      neotest.run.run { strategy = 'dap' }
-    end, { desc = 'Debug nearest' })
-
-    vim.keymap.set('n', '<leader>to', function()
-      neotest.output.open { enter = true, auto_close = true }
-    end, { desc = 'Open output' })
-
-    vim.keymap.set('n', '<leader>tso', function()
-      neotest.summary.open()
-    end, { desc = 'Open summary' })
-
-    vim.keymap.set('n', '<leader>tsc', function()
-      neotest.summary.close()
-    end, { desc = 'Close summary' })
-
-    vim.keymap.set('n', '<leader>tst', function()
-      neotest.summary.close()
-    end, { desc = 'Toggle summary' })
-
-    vim.keymap.set('n', '<leader>tww', function()
-      neotest.watch.watch(vim.fn.expand '%')
-    end, { desc = 'Watch file' })
-
-    vim.keymap.set('n', '<leader>tws', function()
-      ---@diagnostic disable-next-line: missing-parameter
-      neotest.watch.stop()
-    end, { desc = 'Watch file' })
   end,
 }
