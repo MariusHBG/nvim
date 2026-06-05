b36bab4e1b0789ec22b2f4839008de38e9a40691
Marius Gruber <marius_gr@web.de>
2026-06-05 23:47:11 +0200

    debug: remove dap ui

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,47 +1,3 @@
-local function configure_dap_ui()
-  local dap = require 'dap'
-  local dapui = require 'dapui'
-  -- Dap UI setup
-  -- For more information, see |:help nvim-dap-ui|
-  dapui.setup {
-    -- Set icons to characters that are more likely to work in every terminal.
-    --    Feel free to remove or use ones that you like more! :)
-    --    Don't feel like these are good choices.
-    icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-    controls = {
-      icons = {
-        pause = '⏸',
-        play = '▶',
-        step_into = '⏎',
-        step_over = '⏭',
-        step_out = '⏮',
-        step_back = 'b',
-        run_last = '▶▶',
-        terminate = '⏹',
-        disconnect = '⏏',
-      },
-    },
-  }
-
-  dap.listeners.before.attach.dapui_config = function()
-    dapui.open()
-  end
-  dap.listeners.before.launch.dapui_config = function()
-    dapui.open()
-  end
-
-  -- dap.listeners.before.event_terminated.dapui_config = function()
-  --   dapui.close()
-  -- end
-  -- dap.listeners.before.event_exited.dapui_config = function()
-  --   dapui.close()
-  -- end
-
-  -- dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-  -- dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-  -- dap.listeners.before.event_exited['dapui_config'] = dapui.close
-end
-
 local function configure_dap_symbols()
   vim.api.nvim_set_hl(0, 'DapStopped', { fg = '#ffffff', bg = 'gray' })
   vim.api.nvim_set_hl(0, 'DapBreakpoint', { fg = 'red' })
@@ -59,8 +15,6 @@ end
 return {
   'mfussenegger/nvim-dap',
   dependencies = {
-    -- Creates a beautiful debugger UI
-    'rcarriga/nvim-dap-ui',
     {
       'igorlfs/nvim-dap-view',
       -- let the plugin lazy load itself
@@ -69,9 +23,6 @@ return {
       ---@type dapview.Config
       opts = {},
     },
-
-    -- Required dependency for nvim-dap-ui
-    'nvim-neotest/nvim-nio',
 
     -- Installs the debug adapters for you
     'williamboman/mason.nvim',
@@ -178,7 +129,6 @@ return {
       },
     }
 
-    -- configure_dap_ui()
     configure_dap_symbols()
 
     -- Install golang specific config
