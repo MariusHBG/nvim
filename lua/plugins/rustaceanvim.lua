a622efd582d88c314969428baf8a5fd1062a4d13
Marius Gruber <mgruber@quantum-systems.com>
2026-06-15 15:34:43 +0200

    rustaceanvim: configure nightly fmt command

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -2,4 +2,17 @@ return {
   'mrcjkb/rustaceanvim',
   version = '^5', -- Recommended
   lazy = false, -- This plugin is already lazy
+  init = function()
+    vim.g.rustaceanvim = {
+      server = {
+        default_settings = {
+          ['rust-analyzer'] = {
+            rustfmt = {
+              overrideCommand = { 'cargo', '+nightly', 'fmt', '--all' },
+            },
+          },
+        },
+      },
+    }
+  end,
 }
