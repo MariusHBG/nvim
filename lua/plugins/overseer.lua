fa3a421f267953503f3f19cb30311e784de320dc
Marius Gruber <mgruber@quantum-systems.com>
2026-06-22 14:23:27 +0200

    overseer: add pnpm build template that correctly puts errors in quickfix list

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -1,6 +1,24 @@
 return {
   'stevearc/overseer.nvim',
-  opts = {},
+  config = function(_, opts)
+    local overseer = require 'overseer'
+
+    overseer.setup(opts)
+
+    overseer.register_template({
+      name = 'pnpm build',
+      builder = function()
+        return {
+          cmd = { 'pnpm', 'build' },
+          components = {
+            { 'on_output_parse', problem_matcher = '$tsc' },
+            { 'on_result_diagnostics_quickfix', open = true },
+            'default',
+          },
+        }
+      end,
+    })
+  end,
   keys = {
     { '<leader>ot', vim.cmd.OverseerToggle, desc = '[O]verseer [t]oggle' },
     { '<leader>or', vim.cmd.OverseerRun, desc = '[O]verseer [r]un' },
